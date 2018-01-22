bb53fff68f4a7381f48b72b38843b1dfca565303
hammer <tianhanming11@gmail.com>
2018-01-22 20:14:59 +0800

    fix #32, #31 修改爬虫脚本 测试数据

--- scripts/crawl_lagou.py
+++ scripts/crawl_lagou.py
@@ -1,20 +1,30 @@
 import scrapy
 
+
 class JobSpider(scrapy.Spider):
     name = 'jobs'
     start_urls = ['https://www.lagou.com/']
 
     def parse(self, response):
-        for job in response.xpath('.//ul[@class="clearfix position_list_ul"]/li'):
+        for job in response.xpath(
+                './/ul[@class="clearfix position_list_ul"]/li'):
             yield {
-                'name': job.css('div.position_name a::text').extract_first(),
-                'salary': job.css('span.salary ::text').extract_first(),
-                'requirement': job.css('div.position_main_info span::text').extract_first(),
-                'company_name':job.css('div.company_name a::text').extract_first().strip(),
-                'location':job.xpath ( '//div[@class="industry wordCut"]/span[3]/text()' ).extract_first (),
-                'experience': job.css ( 'div.position_main_info span::text' ).extract_first (),
-                'create_date': job.css ( 'span.create-time::text' ).re_first ( '\u2002(.+)\u2002' ),
-                'img_url': job.css('div.clearfix img::attr(src)').extract_first()
+                'name':
+                job.css('div.position_name a::text').extract_first(),
+                'salary':
+                job.css('span.salary ::text').extract_first(),
+                'company_name':
+                job.css('div.company_name a::text').extract_first().strip(),
+                'location':
+                job.xpath('//div[@class="industry wordCut"]/span[3]/text()')
+                .extract_first(),
+                'experience':
+                job.css('div.position_main_info span::text').extract_first(),
+                'degree':
+                job.css('div.position_main_info span:nth-child(2)::text')
+                .extract_first(),
+                'img_url':
+                job.css('div.clearfix img::attr(src)').extract_first()
             }
 
 # scrapy runspider scripts/crawl_lagou.py -o datas/jobs.json
