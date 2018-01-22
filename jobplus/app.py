3364a797282d92b8d588d3c89598d38d79c62bb3
hammer <tianhanming11@gmail.com>
2018-01-22 18:33:24 +0800

    个人信息配置页面

--- jobplus/app.py
+++ jobplus/app.py
@@ -4,19 +4,42 @@ app.py
 """
 from flask import Flask
 from jobplus.config import configs
-from jobplus.models import db
+from jobplus.models import db, User
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
-    db.init_app(app)
     register_blueprints(app)
-    Migrate(app, db)
+    register_extensions(app)
+    create_admin(app)
     return app
 
 
 def register_blueprints(app):
-    from .handlers import front
+    from .handlers import front, user
     app.register_blueprint(front)
+    app.register_blueprint(user)
+
+
+def register_extensions(app):
+    db.init_app(app)
+    Migrate(app, db)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
+
+
+# 创建默认管理员用户
+def create_admin(app):
+    with app.app_context():
+        name, password = User.create_administrator()
+        app.logger.debug('create administrator user: %s, password: %s', name,
+                         password)
