1cfd332b00187ce46500f58b2537ca6f7ab9a26c
hammer <tianhanming11@gmail.com>
2018-01-22 22:28:36 +0800

    添加所有路由 修复bug

--- jobplus/handlers/company.py
+++ jobplus/handlers/company.py
@@ -2,3 +2,14 @@
 company.py
 企业路由函数文件
 """
+from flask import Blueprint, render_template, redirect, url_for, flash
+from jobplus.forms import LoginForm, UserRegisterForm, CompanyRegisterForm
+from jobplus.models import User
+from flask_login import login_user, logout_user, login_required
+
+company = Blueprint('company', __name__, url_prefix='/company')
+
+
+@company.route('/')
+def index():
+    return render_template('companylist.html')
--- jobplus/handlers/job.py
+++ jobplus/handlers/job.py
@@ -2,3 +2,14 @@
 job.py
 职位路由函数文件
 """
+from flask import Blueprint, render_template, redirect, url_for, flash
+from jobplus.forms import LoginForm, UserRegisterForm, CompanyRegisterForm
+from jobplus.models import User
+from flask_login import login_user, logout_user, login_required
+
+job = Blueprint('job', __name__, url_prefix='/job')
+
+
+@job.route('/')
+def index():
+    return render_template('joblist.html')
